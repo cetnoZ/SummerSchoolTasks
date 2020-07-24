fcaa503cbd7250208da4af11f8d354f68dc0e304
Zontec <dehibeo@gmail.com>
2020-07-24 14:55:27 +0300

    all

---
+++ clear.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+rm -f replication_part1/tests/in/*
+rm -f replication_part1/tests/out/*
+
+rm -f replication_part2/tests/in/*
+rm -f replication_part2/tests/out/*
+
+rm -f infection/tests/in/*
+rm -f infection/tests/out/*
+
+rm -f coevolution/tests/in/*
+rm -f coevolution/tests/out/*
+
+rm -f synthesis_and_struggle/tests/in/*
+rm -f synthesis_and_struggle/tests/out/*
