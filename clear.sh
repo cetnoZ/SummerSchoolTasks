a43bd6995dbaf9328754dcee225c20e3192c9850
Valery Kameko <vkamieko@gmail.com>
2020-07-29 18:50:39 +0300

    Modify clear.sh

--- clear.sh
+++ clear.sh
@@ -4,3 +4,10 @@
 ./infection/clear.sh
 ./coevolution/clear.sh
 ./synthesis_and_struggle/clear.sh
+
+make -C ./cactus_queries clear
+make -C ./path_rotation clear
+make -C ./stripe_paths clear
+make -C ./stripe_paths_2 clear
+make -C ./tree_balancing clear
+
